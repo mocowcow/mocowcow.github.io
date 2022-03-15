7efc855485f6f53658f8a1439a1be279782b94ef
ariduki <ariduki@gmail.com>
2022-03-15 17:53:00 +0800

    更新功能：可以連續新增並檢查是否寫過題解

--- _posts/new-leetcode-post.py
+++ _posts/new-leetcode-post.py
@@ -1,27 +1,51 @@
 from datetime import datetime
 import os
 
-# TOPIC = '1. Two Sum'
-TOPIC = input()
+# 找出已經寫過的
+leetcode_posts = set()
+path = '.'
+files = os.listdir(path)
 
-number = TOPIC[:TOPIC.index('.')]
-title = '-'.join(TOPIC.split(' ')[1:]).lower()
-time = datetime.now().strftime('%Y-%m-%d')
-filename = f'{time}-leetcode-{number}-{title}.md'
-template = f"""---
-layout      : single
-title       : LeetCode {TOPIC}
-tags 		: LeetCode
----
-# 題目
+for f in files:
+    try:
+        ss = f.split('-')
+        if ss[3] != 'leetcode':
+            continue
+        n = ss[4]
+        leetcode_posts.add(n)
+    except:
+        pass
 
-# 解法
+# 嘗試新建
+print('目前已寫過', len(leetcode_posts), '題')
+print('輸入題號+題目名：\n')
+while True:
+    # TOPIC = '1. Two Sum'
+    TOPIC = input().strip()
+    number = TOPIC[:TOPIC.index('.')]
+    if number in leetcode_posts:
+        print('已經寫過了\n')
+        continue
+    title = '-'.join(TOPIC.split(' ')[1:]).lower()
+    time = datetime.now().strftime('%Y-%m-%d')
+    filename = f'{time}-leetcode-{number}-{title}.md'
+    template = f"""
+                ---
+                layout      : single
+                title       : LeetCode {TOPIC}
+                tags 		: LeetCode
+                ---
+                # 題目
 
-```python
-code here
-```
-"""
-if not os.path.exists(f'./{filename}'):
-    print('file created')
+                # 解法
+
+                ```python
+                code here
+
+                ```
+
+                """
+
+    print('新建成功\n')
     f = open(filename, 'w', encoding='utf8')
     f.write(template)
