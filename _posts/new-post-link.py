5a9e5f4a4d3b39b82c4dce9b1c5835a8e3a6d013
ariduki <ariduki@gmail.com>
2022-04-26 13:37:01 +0800

    add new-post-link.py

---
+++ _posts/new-post-link.py
@@ -0,0 +1,16 @@
+from string import Template
+
+
+# 輸入文章的檔名(不含.md)
+# link = '2022-04-26-leetcode-940-distinct-subsequences-ii'
+link = input()
+try:
+    title = link.split('leetcode-')[1].replace('-', ' ').replace(' ', '. ', 1)
+except:
+    title = 'invalid_title'
+
+template = Template('[$TITLE]({% post_url $LINK %})')
+s = template.substitute(TITLE=title, LINK=link)
+print('\n\n\n\n\n', s, '\n\n\n\n\n')
+
+
