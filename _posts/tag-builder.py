b6c01b58363ab9bc655a846f1a18a05bd2aed281
ariduki <ariduki@gmail.com>
2022-05-01 16:05:33 +0800

    add tag builder.py

---
+++ _posts/tag-builder.py
@@ -0,0 +1,57 @@
+d = {
+
+    'lc':  'LeetCode',
+    'm':  'Medium',
+    'a':  'Array',
+    'e':  'Easy',
+    'dp':  'DP',
+    'h':  'Hard',
+    'bs':  'BinarySearch',
+    's':  'String',
+    'gd':  'Greedy',
+    'sort':  'Sorting',
+    'tp':  'TwoPointers',
+    'dfs':  'DFS',
+    'math':  'Math',
+    'ht':  'HashTable',
+    'mat':  'Matrix',
+    'bfs':  'BFS',
+    'g':  'Graph',
+    'ps':  'PrefixSum',
+    'design':  'Design',
+    'st':  'Stack',
+    'll':  'LinkedList',
+    'bit':  'BitManipulation',
+    'hp':  'Heap',
+    'bintree':  'BinaryTree',
+    'sw':  'SlidingWindow',
+    'XXXX':  'MonotonicStack',
+    'bt':  'Backtracking',
+    'segtree':  'SegmentTree',
+    'bst':  'BinarySearchTree',
+    'trie':  'Trie',
+    'sim':  'Simulation',
+    'count':  'Counting',
+    'geo':  'Geometry',
+    'XXXX':  '筆記',
+    'daq':  'DevideAndConquer',
+    'tree':  'Tree',
+    'sl':  'SortedList',
+    'XXX':  'Python',
+    'mst':  'MST',
+    'uf':  'UnionFind'
+}
+
+
+st = []
+while True:
+    x = input()
+    if x == '':
+        break
+    elif x in d:
+        st.append(d[x])
+    else:
+        print(x, 'not found')
+    print(st)
+
+print(' '.join(st))
